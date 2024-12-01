c351de93856894668b9a8cd9e3475c6c74a0ca80
Alejo Perea <mpereamejia@gmail.com>
2024-12-01 17:03:11 -0500

    Creacion de las clases console, debug, file en el proyecto NorthWind.Writers

--- NorthWind.ConsoleApp/Program.cs
+++ NorthWind.ConsoleApp/Program.cs
@@ -1,7 +1,22 @@
 ﻿
 using NorthWind.ConsoleApp.Services;
 using NorthWindd.Entities.Interfaces;
+using NorthWind.Writers;
 
-IUserActionWriter Writer = default;
+//IUserActionWriter Writer = new ConsoleWriter();
+//IUserActionWriter Writer = new DebugWriter();
+IUserActionWriter Writer = new FileWriter();
+
 AppLogger Logger = new AppLogger(Writer);
 Logger.WriteLog("Application started.");
+
+ProductService Service = new ProductService(Writer);
+Service.Add("Demo", "Azucar refinada");
+
+/*
+ * AppLoger y los Writers:Responsabilidad Unica
+ * AppLogger: Abierto pero cerrado
+ * AppLogger: Inversion de depencias. Los modulos de alto nivel son independiente de los detalles de implementacion
+ * 
+ * 
+ */
---
+++ NorthWind.Writers/GlobalUsings.cs
@@ -0,0 +1,8 @@
+global using NorthWindd.Entities.Interfaces;
+global using NorthWindd.Entities.ValueObjects;
+global using System;
+global using System.Collections.Generic;
+global using System.Diagnostics;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
